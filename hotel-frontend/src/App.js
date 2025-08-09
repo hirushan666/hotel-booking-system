67fedba6bfc8bf7e48972425562e76d6f665f8bd
hirushan666 <hirushanww@gmail.com>
2025-08-09 14:33:46 +0530

    Remove hotel-frontend submodule, track as normal folder

---
+++ hotel-frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HotelList from './HotelList';
+
+function App() {
+  return (
+    <div>
+      <HotelList />
+    </div>
+  );
+}
+
+export default App;
---
+++ hotel-frontend/src/RoomList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function RoomList({ rooms }) {
+  if (!rooms.length) return <p>No rooms available.</p>;
+
+  return (
+    <div>
+      <h2>Rooms</h2>
+      <ul>
+        {rooms.map(room => (
+          <li key={room.id}>
+            Room {room.roomNumber} - {room.type} - ${room.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default RoomList;
